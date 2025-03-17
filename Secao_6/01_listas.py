e19dac18cedf9e3fb88aaaf36ba80035e6391e45
Daniel Moscardini <moscardinibdaniel@gmail.com>
2025-03-17 20:38:09 -0300

    adicionando alteracoes pos ferias

---
+++ Secao_6/01_listas.py
@@ -0,0 +1,18 @@
+"""
+Vetores e Matrizes sao dinamicos e podem ser de qualquer tipo.
+Dinamicos: Nao possuem tamanho fixo, podemos criar a lista e ir adicionando elementos.
+
+As listas em Python sao representadas por colchetes: []
+"""
+
+lista1 = [1, 4, 6, 10, 3, 4]
+lista2 = ['a', 'G', 'u', '!', 'r', '?']
+
+# Checa se determinado valor esta contido na lista:
+num = 10
+if num in lista1:
+    print('Existe numero 10 na lista')
+else:
+    print('Nao existe numero 10 na lista')
+
+
